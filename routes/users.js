b5377fb0669c77b76fb4b25c20a2f05b0ed0ceca
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-10-05 11:01:27 -0700

    added login-test route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login-test', (req,res) => res.send({
+  firstname: 'Paul',
+  lastname: 'Atreides',
+  email: 'paul@arrakis.com'
+}))
+
 module.exports = router;
