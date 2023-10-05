beee427886c3f0b7216f4fc5fcf30a58b3450aac
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-10-05 10:03:57 -0700

    successfully connecting to MongoDb

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, {dbName: process.env.DB_NAME})
+        .then(() => console.log('MongoDB Connected!!! Yea!!!!'))
+        .catch((err) => console.log(err))
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +27,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
