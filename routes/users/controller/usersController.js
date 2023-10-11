792b7867a7b9aa39c9fea4fc94827808f2060962
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-10-11 10:59:21 -0700

    login save jwt token, set expiration from remember button on front end

--- routes/users/controller/usersController.js
+++ routes/users/controller/usersController.js
@@ -31,6 +31,7 @@ module.exports = {
                 firstname: savedUser.firstname,
                 lastname: savedUser.lastname,
                 message: "Successfully Registered"
+                //token
             })
 
         } catch (error) {
@@ -67,11 +68,29 @@ module.exports = {
             //     message: "Successful Login!"
             // })
             
+            // payload should be a unique key to look up user in the DB
+            let payload = {
+                id: foundUser._id,
+                // email: founderUser.email
+            }
+
+            //expiration like banking sites, will expire after 15 mins, unless remember box is checked.
+            let expiration = new Number
+            if (req.body.isRemember) {
+                expiration = 60*60*24*7
+            } else {
+                expiration = 60*15
+            }
+            
+
+            let token = await jwt.sign(payload, process.env.SUPER_SECERT_KEY, {expiresIn: expiration})
+
             res.status(200).json({
                 email: foundUser.email,
                 firstname: foundUser.firstname,
                 lastname: foundUser.lastname,                   
-                message: "Successful Login!"
+                message: "Successful Login!",
+                token: token
             })
 
             
